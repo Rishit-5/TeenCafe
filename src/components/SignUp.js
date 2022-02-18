5e7500ada53bc88520564378fea704ddee40659e
Thanos <58965520+Sukoia@users.noreply.github.com>
2022-02-17 18:40:17 -0700

    Added password confirm auth (pass and confirm pass must match)

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,42 +1,75 @@
-import React, {Component, useState} from 'react';
-import Carousel from "../BootstrapCarousel";
+import React, {useState, useRef} from 'react';
 import {Form, Button} from "react-bootstrap";
+import { Link, useHistory } from "react-router-dom"
+
+/* import { AuthProvider } from "../contexts/AuthContext"
+   import { useAuth } from "../contexts/AuthContext" 
+*/
+
 const SignUp = () => {
+
+    const passwordRef = useRef()
+    const passwordConfirmRef = useRef() 
+    /* const { signup } = useAuth() */
+    const [error, setError] = useState("")
+    const [loading, setLoading] = useState(false)
+    const history = useHistory()
+
+    async function handleSubmit(e) {
+        e.preventDefault()
+    
+        if (passwordRef.current.value !== passwordConfirmRef.current.value) {
+          return setError("Passwords do not match")
+        }
+    
+        try {
+          setError("")
+          setLoading(true)
+          await passwordRef.current.value /* wrap it in signup() for firebase auth ++ <AuthProvider></AuthProvider> in return value below*/
+          history.push("/")
+        } catch {
+          setError("Failed to create an account")
+        }
+    
+        setLoading(false)
+      }
+
+
     return (
-        <div>
+            <div>
             <br />
             <br />
             <br />
             <h1 className="mb-3">Sign Up</h1>
-            <Form className="mt-30 w-50 mx-auto">
+            <Form className="mt-30 w-50 mx-auto" onSubmit={handleSubmit}>
                 <Form.Group className="mb-3" controlId="formBasicEmail">
                     <Form.Label>First Name</Form.Label>
-                    <Form.Control placeholder="First Name" />
+                    <Form.Control type="text" placeholder="First Name" required/>
                 </Form.Group>
 
                 <Form.Group className="mb-3" controlId="formBasicEmail">
                     <Form.Label>Last Name</Form.Label>
-                    <Form.Control placeholder="Last Name" />
+                    <Form.Control type="text" placeholder="Last Name" required/>
                 </Form.Group>
 
                 <Form.Group className="mb-3" controlId="formBasicEmail">
                     <Form.Label>Address</Form.Label>
-                    <Form.Control placeholder="Address" />
+                    <Form.Control type="text" placeholder="Address" required/>
                 </Form.Group>
 
                 <Form.Group className="mb-3" controlId="formBasicEmail">
                     <Form.Label>Phone Number</Form.Label>
-                    <Form.Control placeholder="Phone Number" />
+                    <Form.Control type="tel" placeholder="Phone Number" required/> 
                 </Form.Group>
 
                 <Form.Group className="mb-3" controlId="formBasicPassword">
                     <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" placeholder="Password" />
+                    <Form.Control type="password" ref={passwordRef} placeholder="Password" required />
                 </Form.Group>
 
                 <Form.Group className="mb-3" controlId="formBasicPassword">
                     <Form.Label>Confirm Password</Form.Label>
-                    <Form.Control type="password" placeholder="Confirm Password" />
+                    <Form.Control type="password" ref={passwordConfirmRef} placeholder="Confirm Password" required />
                 </Form.Group>
                 <Button variant="primary" type="submit">
                     Submit
@@ -45,10 +78,9 @@ const SignUp = () => {
             <br />
             <br />
             <br />
-            
-
         </div>
-      
+
     )
 }
 export default SignUp;
+
---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+/* Remember to create the env file and set them to REACT_APP_FIREBASE_API_KEY=...,REACT_APP_FIREBASE_AUTH_DOMAIN=..., etc*/
+
+import firebase from "firebase/app"
+import "firebase/compat/auth"
+
+const app = firebase.initializeApp({
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
+})
+
+export const auth = app.auth()
+export default app
