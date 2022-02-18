5966a17fee55dceda438733a17cde30453e459b1
7riumph <58965520+7riumph@users.noreply.github.com>
2022-02-18 02:04:07 -0700

    bug testing

--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -15,26 +15,6 @@ export function AuthProvider({ children }) {
     return auth.createUserWithEmailAndPassword(email, password)
   }
 
-  function login(email, password) {
-    return auth.signInWithEmailAndPassword(email, password)
-  }
-
-  function logout() {
-    return auth.signOut()
-  }
-
-  function resetPassword(email) {
-    return auth.sendPasswordResetEmail(email)
-  }
-
-  function updateEmail(email) {
-    return currentUser.updateEmail(email)
-  }
-
-  function updatePassword(password) {
-    return currentUser.updatePassword(password)
-  }
-
   useEffect(() => {
     const unsubscribe = auth.onAuthStateChanged(user => {
       setCurrentUser(user)
@@ -46,12 +26,7 @@ export function AuthProvider({ children }) {
 
   const value = {
     currentUser,
-    login,
     signup,
-    logout,
-    resetPassword,
-    updateEmail,
-    updatePassword
   }
 
   return (
