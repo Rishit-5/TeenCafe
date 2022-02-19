66c854559cb498763dce0dc983dd3282686c606e
Rishit-5 <rishistar05@gmail.com>
2022-02-18 18:59:23 -0600

    hosting stuff v14

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import BootstrapCarousel from './BootstrapCarousel';
 import BootstrapNavbar from "./BootstrapNavbar";
-import {BrowserRouter as Router, Route, Link, BrowserRouter, Switch, HashRouter} from 'react-router-dom';
+import {BrowserRouter as Router, Route, Link, BrowserRouter, Switch} from 'react-router-dom';
 import Home from "./components/Home"
 import SignUp from "./components/SignUp";
 import Footer from "./components/footer"
@@ -9,16 +9,16 @@ import Footer from "./components/footer"
 function App() {
     return (
             <div className="App">
-                <HashRouter basename="/TeenCafe">
+                <BrowserRouter basename="/TeenCafe">
                     <BootstrapNavbar />
                         <div className="pl-20">
                     <Switch>
                         <Route exact path='/' component={Home} />
-                        <Route path='/TeenCafe/signup' component={SignUp} />
+                        <Route path='/signup' component={SignUp} />
                     </Switch>
                         </div>
                     <Footer />
-                </HashRouter>
+                </BrowserRouter>
             </div>
 
     );
